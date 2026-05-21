4a07486d3182b9e3f871f14cdc5b5d6b3a954591
Jan Sváda <jan.svada@setio.cz>
2026-05-21 15:23:48 +0200

    Track production web runtime config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
       name: "vendy-reality-web",
       cwd: "/var/www/nisacentrum/vendy-reality-web",
       script: "node_modules/next/dist/bin/next",
-      args: "start -p 3000",
+      args: "start -p 3000 -H 127.0.0.1",
       exec_mode: "fork",
       instances: 1,
       autorestart: true,
